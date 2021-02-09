e37e74e29f4ea17266973a0802a1854bcd104604
crxlunch <chairulrahmant@gmail.com>
2021-02-09 15:05:45 +0700

    Version 0.1.3

--- sosmed/src/main/java/com/kkds/sosmed/SosmedIgClient.kt
+++ sosmed/src/main/java/com/kkds/sosmed/SosmedIgClient.kt
@@ -6,9 +6,12 @@ import com.sanardev.instagramapijava.IGConstants
 import com.sanardev.instagramapijava.InstaClient
 import com.sanardev.instagramapijava.app.Cookie
 import com.sanardev.instagramapijava.model.timeline.FeedItems
+import com.sanardev.instagramapijava.model.timeline.MediaOrAd
+import com.sanardev.instagramapijava.model.user.BigUser
 import com.sanardev.instagramapijava.processor.*
 import com.sanardev.instagramapijava.request.IGLoginRequest
 import com.sanardev.instagramapijava.response.IGLoginResponse
+import com.sanardev.instagramapijava.response.IGPostsResponse
 import com.sanardev.instagramapijava.response.IGTimeLinePostsResponse
 import com.sanardev.instagramapijava.response.IGUserInfoResponse
 import com.sanardev.instagramapijava.utils.InstaHashUtils
@@ -39,6 +42,55 @@ public class SosmedIgClient {
     fun getUserInfoByUsername(username: String) :Observable<IGUserInfoResponse>{
         return instaClient.userProcessor.getUserInfoByUsername(username)
     }
+
+    fun getUserFeed(pk: Long): Observable<IGPostsResponse>{
+        return instaClient.userProcessor.getPosts(pk)
+    }
+
+    fun reloadMoreDataFeed(pk: Long,id: String): Observable<IGPostsResponse> {
+        return instaClient.userProcessor.getMorePosts(pk, id)
+    }
+
+    fun saveDataToInstagramFeeds(listPostFromAPI: List<MediaOrAd>, user: BigUser) : ArrayList<InstagramFeed> {
+        var listFeed = ArrayList<InstagramFeed>()
+        var isCanReaload = false
+        var lastId = ""
+        var url = URLDecoder.decode(user.hdProfilePicUrlInfo.url, "UTF-8")
+        listPostFromAPI
+            .forEach {
+                if(it.clientCacheKey.isNullOrBlank())it.clientCacheKey = "-"
+                var newFeed =
+                    InstagramFeed(
+                        id = it.id,
+                        pk = user.pk,
+                        username = user.username,
+                        profilePic = url,
+                        caption = it.caption?.text,
+                        url = if (it.carouselMediaCount > 0) it.carouselMedias.first().imageVersions2.candidates.first().url else URLDecoder.decode(
+                            it.imageVersions2.candidates.last().url,
+                            "UTF-8"
+                        ),
+                        devTime = it.deviceTimestamp,
+                        height = it.originalHeight,
+                        width = it.originalHeight,
+                        likeCount = it.likeCount
+                    )
+
+                listFeed.add(newFeed)
+                Log.v("CRX", "Size Feed " + listFeed.size)
+
+                if(it == listPostFromAPI.last()){
+                    //if(listFeed.size > 20) {
+                    //isCanReaload = true
+                    lastId = it.id
+                    Log.v("CRX", "Feed Last")
+                }
+            }
+
+        return listFeed
+
+    }
+
 
     fun getTimelinePosts(): Observable<IGTimeLinePostsResponse>{
         return  instaClient.mediaProcessor.timelinePosts
